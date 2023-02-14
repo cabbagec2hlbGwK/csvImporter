1f692cb71c9656fbab99e2e3ec94658a02d8829a
cabbagec2hlbGwK <cabbageshell@protonmail.com>
2023-02-14 15:37:39 -0500

    the function to create the query for the create table is DONE

--- importCSV.py
+++ importCSV.py
@@ -6,30 +6,54 @@ import pyodbc as odb
 
 
 def otherType(data):
+    datePattern = r"\d{4}[-/]\d{2}[-/]\d{2}"
     print(data)
+    if re.match(datePattern, data):
+        if re.match(r".*([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$", data):
+            return "DATETIME"
+        return "DATE"
+    elif re.match("^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$", data):
+        return "TIMESTAMP"
     return "VARCHAR"
 
 
-def typeGess(headers):
-    print(headers)
+def typeGess(df) -> dict():
     typeMapping = {}
-    for head, type in headers:
+    for head, type in df.dtypes.items():
         dataType = None
         if type == pandas.Int64Dtype.type:
             dataType = "INTEGER"
         elif type == pandas.Float64Dtype.type:
             dataType = "FLOAT"
+        elif type == "bool":
+            dataType = "BOOLEAN"
         else:
-            dataType = otherType(head)
+            print(type)
+            dataType = otherType(df[head][1])
         typeMapping[head] = dataType
 
-    print(typeMapping)
+    return typeMapping
+
+# creting the query for the create statement if the table name is not passed
+
+
+def createQuery(mapper):
+    size = len(mapper)
+    createQuery = "CREATE TABLE IF NOT EXISTS employees ("
+    for i, (column, type1) in enumerate(mapper.items()):
+        createQuery += f"{column.replace(' ','_')} {type1}"
+        if i != size-1:
+            createQuery += " ,"
+    createQuery += " );"
+    return createQuery
 
 
 def main():
     SEP = os.path.sep
     df = pandas.read_csv("quality.csv", sep=";")
-    mapper = typeGess(df.dtypes.items())
+    mapper = typeGess(df)
+    tableQuery = createQuery(mapper)
+    print(tableQuery)
 
 
 if __name__ == "__main__":
